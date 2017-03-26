5518bdd8ab1d07fa048f03a6999efca7303a31f9
MP <id@id.com>
2017-03-26 12:19:20 -0500

    Programming Assignment 2 functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow us to create matrix objects that cache their inverse and 
+## return the cached inverse. These matrices improve the performance of 
+## calculations where reversal matrix is done many items. 
 
-## Write a short comment describing this function
-
+## A special "matrix" object that can hold cache of its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    r <- NULL
+    
+    set <- function(y) {
+            x <<- y
+            r <<- NULL
+    }
+    get <- function() x
+    
+    setreverse <- function(reverse) r <<- reverse
+    getreverse <- function() r
+    
+    list(set = set, get = get,
+         setreverse = setreverse,
+         getreverse = getreverse)
 }
 
-
-## Write a short comment describing this function
-
+## Function that retrieves the cached instance of reverse matrix. 
+## It creates and cahces a reverse matrix instance it if it doesn't exist in the cache. 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Returns a matrix that is the inverse of 'x'
+    r <- x$getreverse()
+    if(!is.null(r)) {
+        message("getting cached data")
+        return(r)
+    }
+    
+    data <- x$get()
+    r <- solve(data)
+    x$setreverse(r)
+    
+    message("cached reverse matrix")
+    r    
 }
